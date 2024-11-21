bff657ffa3ebc197d4d1069e8a52ccc348d723ed
zim89 <121241841+zim89@users.noreply.github.com>
2024-11-21 17:41:59 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# tik-tak-toe-online
+ 
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/shared/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
+}
